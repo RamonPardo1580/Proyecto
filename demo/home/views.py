9d347fceaa654c58ff627a551164dca361844f87
RamonPardo1580 <60410394+RamonPardo1580@users.noreply.github.com>
2021-06-06 14:59:23 -0700

    Delete demo directory

--- demo/home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render (request, "home/index.html",{})
-
-def segunda(request):
-    return render (request, "home/segunda.html",{})
